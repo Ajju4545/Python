0e38ed8ff50d0537db5a03869f49d1c911f097c4
Ajay <your_email@gmail.com>
2026-03-12 11:29:20 +0530

    update file

--- Arithmetic_Operator.py
+++ Arithmetic_Operator.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 a = 10
 b = 3
 
@@ -7,4 +8,15 @@ print("Multiplication:", a * b)
 print("Division:", a / b)
 print("Modulus:", a % b)
 print("Power:", a ** b)
+=======
+a = 10
+b = 3
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Power:", a ** b)
+>>>>>>> 6d0e57d32eafd0883f3a5f1e2bf1cf75c23faf10
 print("Floor Division:", a // b)
